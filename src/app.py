916ad4c0c188cfec3ac2033833eb57fc1f7c6961
monsantoshi <monsan@gmail.com>
2024-02-19 17:08:07 +0700

    CC

--- src/app.py
+++ src/app.py
@@ -6,13 +6,13 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from dotenv import load_dotenv
+
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
-load_dotenv()
+API_KEY = st.secrets['OPENAI_API_KEY']
 
 def get_vectorstore_from_url(url):
     # get the text in document form
